584a2cf9da2265a639efe57c6665b70810511ef7
Mubarak Al Hmood <39665871+mralhmood@users.noreply.github.com>
2019-05-28 22:38:30 -0700

    Update models.js
    my eye's lids are closing!!

--- backend/models.js
+++ backend/models.js
@@ -1,6 +1,59 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost/foodapplication', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/yumyums', {
+  useNewUrlParser: true
+});
+
+const YumyumSchema = new mongoose.Schema({
+    yumyum: {
+    type: String,
+    required: [true, "give your yumyum a title"],
+    min: [3, 'true real yumyums need to be 3 letters and more']
+    },
+    date: {
+        type: Date,
+        required: [false, "if its not in your pocket list either delete it or tick pocket list"],
+    },
+    times: {
+        type: String,
+        required: [false, "if its not in your pocket list either delete it or tick pocket list"],
+      },
+  likes: {
+    type: Number,
+    default: 0,
+    },
+    plist: {
+        type: Boolean,
+        default: false,
+      },
+  reator: [UserSchema],
+
+  restaurant: [TestaurantSchema],
+
+  rsvp: [RSVPSchema],
+}, { timestamps: true });
 
 
-// // module.exports = mongoose.model('Object', ObjectSchema);
+const UserSchema = new mongoose.Schema({
+    // firstName: string;
+    // lastName: string;
+    // email: string;
+    // password: string;
+    // confirmed: string;
+    // tel: number;
+    // street1: string;
+    // street2: string;
+    // city: string;
+    // zip: number;
+    // country: string;
+  firstName: {
+    type: String,
+    required: [true, "You cant be nameless, if you are! use infamous as your first name; unless you are Chuck Norris!"],
+    min: 3
+  },
+},
+    { timestamps: true });
+
+module.exports = mongoose.model('YumYum', YumyumSchema);
+
+//i'll continue later .. time to sleep
